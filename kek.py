f9dab4f19357ccd43f807b121296c8abaccc0fec
nice354 <57825694+nice354@users.noreply.github.com>
2019-11-17 10:39:19 +0300

    Update kek.py

--- kek.py
+++ kek.py
@@ -48,20 +48,20 @@ while True:
 
 
         if text.lower() == "привет":
-            vk.method("messages.send", {"peer_id": id, "message": "Привет!", "random_id": random.randint(1, 999999)})
-        elif text == "клавиатура":
+            vk.method("messages.send", {"peer_id": id, "message": "Я здесь!", "random_id": random.randint(1, 999999)})
+        elif text == "Клавиатура":
             vk.method("messages.send", {"peer_id": id, "message": "Держи свою клаву!", "random_id": random.randint(1, 999999), "keyboard": keyboard})
         elif text == "Инфо":
-            vk.method("messages.send", {"peer_id": id, "message":  "Я бот Витёк, могу дать мотивирующий совет.", "random_id": random.randint(1, 999999)})
+            vk.method("messages.send", {"peer_id": id, "message":  "Здравствуй, друг я бот Витёк, могу дать мотивирующий совет. Команда моих создателей называется Vitech", "random_id": random.randint(1, 999999)})
         elif text == "Дать совет":
             vk.method("messages.send", {"peer_id": id, "message": "Единственной уважительной причиной, по которой вы можете сдаться, является ваша смерть. До тех пор, пока вы живы (здоровы и свободны), у вас есть выбор, чтобы совершать попытки до окончательного успеха.", "random_id": random.randint(1, 999999)})
         elif text == "Дать ещё совет":
             vk.method("messages.send", {"peer_id": id, "message": "Вероятность освоения чего-то с первого раза очень мала. Все требует времени, чтобы этому научиться, и вы будете совершать ошибки. Учитесь на них.", "random_id": random.randint(1, 999999)})
         elif text == "Дать третий совет":
             vk.method("messages.send", {"peer_id": id, "message": "Зачастую, когда вы чувствуете, что хотите сдаться, вы находитесь так близко к тому, чтобы сделать огромный прорыв. В любой момент своего времени вы всегда находитесь лишь на волосок от успеха.", "random_id": random.randint(1, 999999)})
-        elif text.lower() == "пока":
+        elif text == "Пока":
             vk.method("messages.send", {"peer_id": id, "message": "Пока :(", "random_id": random.randint(1, 999999)})
-        elif text.lower() == "как дела":
+        elif text == "Как дела":
             vk.method("messages.send", {"peer_id": id, "message": "Бот Витёк негодует D:", "random_id": random.randint(1, 999999)})
         elif text == "Дать четвёртый совет":
             vk.method("messages.send", {"peer_id": id, "message": "Когда вы достигнете всего, чего намеревались достичь, вы можете использовать свой успех, чтобы совершать изменения в мире или в жизни отдельных людей.", "random_id": random.randint(1, 999999)})
@@ -69,9 +69,47 @@ while True:
             vk.method("messages.send", {"peer_id": id, "message": "Если вы не хотите быть известным, как кто-то другой, то это слабость, и говорит о том, что вы сдаетесь. Выходите и показывайте себя остальному миру, а также самому себе. Вы можете и добьетесь того, что намеревались сделать. Вы потерпите неудачу только тогда, когда сдадитесь.", "random_id": random.randint(1, 999999)})
         elif text == "Поддеражать автора":
             vk.method("messages.send", {"peer_id": id, "message": "89600404465 киви", "random_id": random.randint(1, 999999)})
-        elif text.lower() == "где ты работаешь":
+        elif text == "Где ты работаешь":
             vk.method("messages.send", {"peer_id": id, "message": "Я работаю в Байтике.", "random_id": random.randint(1, 999999)})
         elif text == "Дать весёлый совет :D":
             vk.method("messages.send", {"peer_id": id, "message": "Будь как протон: оставайся позитивным!", "random_id": random.randint(1, 999999)})
+        elif text == "Витёк":
+            vk.method("messages.send", {"peer_id": id, "message": "Да это я, бот Витёк", "random_id": random.randint(1, 999999)})
+        elif text == "Какую еду ты любишь":
+            vk.method("messages.send", {"peer_id": id, "message": "Я люблю роллы, но это не всё.", "random_id": random.randint(1, 999999)})
+        elif text == "Какие игры ты любишь":
+            vk.method("messages.send", {"peer_id": id, "message": "Я люблю змейку.", "random_id": random.randint(1, 999999)})
+        elif text == "Помощь":
+            vk.method("messages.send", {"peer_id": id, "message": "Вот мои команды:1 напиши клавиатура.2 Нажми на кнопку и получи ответ.3 задай свои вопрос по типу: где ты работаешь ", "random_id": random.randint(1, 999999)})
+        elif text == "Что ты любишь":
+            vk.method("messages.send", {"peer_id": id, "message": "Я люблю Гвидона вана Россума", "random_id": random.randint(1, 999999)})
+        elif text == "Корпорация зла":
+            vk.method("messages.send", {"peer_id": id, "message": "Apple", "random_id": random.randint(1, 999999)})
+        elif text == "Спасибо":
+            vk.method("messages.send", {"peer_id": id, "message": "Спасибо в карман не положишь (кнопка Поддеражать автора)", "random_id": random.randint(1, 999999)})
+        elif text == "Ок":
+            vk.method("messages.send", {"peer_id": id, "message": "oll korrect- Википедия.", "random_id": random.randint(1, 999999)})
+        elif text == "Посоветуй канал на ютубе":
+            vk.method("messages.send", {"peer_id": id, "message": "Руслан Гительман", "random_id": random.randint(1, 999999)})
+        elif text == "Господи":
+            vk.method("messages.send", {"peer_id": id, "message": "Роберт Эллиот Кан?", "random_id": random.randint(1, 999999)})
+        elif text == "Ты бот":
+            vk.method("messages.send", {"peer_id": id, "message": "Я настоящий живой мужчина. Забрался в ваше устройство, сижу.", "random_id": random.randint(1, 999999)})
+        elif text == "Салам":
+            vk.method("messages.send", {"peer_id": id, "message": "Кто меня звал?", "random_id": random.randint(1, 999999)})
+        elif text == "Freelance BOT":
+            vk.method("messages.send", {"peer_id": id, "message": "Да, слышал про это убожество", "random_id": random.randint(1, 999999)})
+        elif text == "Кто такой бот Олег":
+            vk.method("messages.send", {"peer_id": id, "message": "https://github.com/Draqneel", "random_id": random.randint(1, 999999)})
+        elif text == "Посоветуй песню":
+            vk.method("messages.send", {"peer_id": id, "message": "DEUTSCHLAND by Rammstein", "random_id": random.randint(1, 999999)})
+        elif text == "Скажи 300":
+            vk.method("messages.send", {"peer_id": id, "message": "Слишком банально!", "random_id": random.randint(1, 999999)})
+        elif text == "150+150":
+            vk.method("messages.send", {"peer_id": id, "message": "Меня так просто не обманешь!", "random_id": random.randint(1, 999999)})
+        elif text == "600:2":
+            vk.method("messages.send", {"peer_id": id, "message": "Триста. Да, мы трактористы))", "random_id": random.randint(1, 999999)})
+        elif text == "Типичный малолетний...":
+            vk.method("messages.send", {"peer_id": id, "message": "Типичный плюс", "random_id": random.randint(1, 999999)})
         else:
             vk.method("messages.send", {"peer_id": id, "message": "Извените, вы скорее всего ошиблись","random_id": random.randint(1, 999999)})
